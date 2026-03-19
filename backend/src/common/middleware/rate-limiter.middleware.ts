fd88631ec8da3892e8ef92c033dbc14cf1cc4f29
Bikash Shaw <vshaw138@gmail.com>
2026-03-19 06:47:06 +0530

    feat: Implement initial backend rate limiting

---
+++ backend/src/common/middleware/rate-limiter.middleware.ts
@@ -0,0 +1,49 @@
+import { RateLimiterRedis } from "rate-limiter-flexible";
+import { redis } from "../../lib/redis";
+import type { Request, Response, NextFunction } from "express";
+import { AppError } from "../errors/app.error";
+import { ErrorCode } from "../errors/error.types";
+
+const generalLimiter = new RateLimiterRedis({
+  storeClient: redis,
+  keyPrefix: "general_limit",
+  points: 10,
+  duration: 1,
+});
+
+const authLimiter = new RateLimiterRedis({
+  storeClient: redis,
+  keyPrefix: "auth_limit",
+  points: 2,
+  duration: 60 * 10,
+});
+
+const searchLimiter = new RateLimiterRedis({
+  storeClient: redis,
+  keyPrefix: "search_limit",
+  points: 20,
+  duration: 60,
+});
+
+const createRateLimitMiddleware = (limiter: RateLimiterRedis) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    console.log("ENtered" + limiter);
+    limiter
+      .consume(req.ip!)
+      .then(() => {
+        next();
+      })
+      .catch((rejRes) => {
+        const seconds = Math.round(rejRes.msBeforeNext / 1000) || 1;
+        const message =
+          seconds > 60
+            ? `Retry after ${Math.ceil(seconds / 60)} minutes`
+            : `Retry after ${seconds} seconds`;
+        return next(new AppError(message, 429, ErrorCode.RATE_LIMIT_REACHED));
+      });
+  };
+};
+
+export const generalRateLimit = createRateLimitMiddleware(generalLimiter);
+export const authRateLimit = createRateLimitMiddleware(authLimiter);
+export const searchRateLimit = createRateLimitMiddleware(searchLimiter);
--- backend/src/modules/auth/auth.routes.ts
+++ backend/src/modules/auth/auth.routes.ts
@@ -6,24 +6,28 @@ import {
   createSignupSchema,
   createAdminSigninSchema,
 } from "./auth.validation";
+import { authRateLimit } from "../../common/middleware/rate-limiter.middleware";
 
 const authRoute = Router();
 const authHandler = new AuthHandler();
 
 authRoute.post(
   "/signin",
+  authRateLimit,
   validateRequest(createSigninSchema),
   authHandler.createSignin,
 );
 
 authRoute.post(
   "/signup",
+  authRateLimit,
   validateRequest(createSignupSchema),
   authHandler.createSignup,
 );
 
 authRoute.post(
   "/admin-signin",
+  authRateLimit,
   validateRequest(createAdminSigninSchema),
   authHandler.createAdminSignin,
 );
